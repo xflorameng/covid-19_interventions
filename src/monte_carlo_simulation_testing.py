1492e9b284b426f4385206d7d591a4d14ac17cc5
xflorameng <33183180+xflorameng@users.noreply.github.com>
2021-04-15 13:43:06 -0400

    Update monte_carlo_simulation_testing.py

--- src/monte_carlo_simulation_testing.py
+++ src/monte_carlo_simulation_testing.py
@@ -11,10 +11,10 @@ NUM_TRIALS = 100
 monte_carlo(viral_test_fraction_all=np.around(np.arange(0, .11, 1e-2), decimals=3),
             num_households=NUM_HOUSEHOLDS, max_household_sizes=MAX_HOUSEHOLD_SIZES,
             time_steps_pre_lockdown=180, time_steps_post_lockdown_pre_reopening=0, subsidy=0, num_trials=NUM_TRIALS,
-            dir_name_tag=DIR_NAME_TAG, timestamp=False)
+            dir_name_tag=DIR_NAME_TAG, timestamp=False, path='../results/')
 
 # Robustness test of same-age households
 monte_carlo(same_age_household=True, viral_test_fraction_all=np.around(np.arange(0, .11, 1e-2), decimals=3),
             num_households=NUM_HOUSEHOLDS, max_household_sizes=MAX_HOUSEHOLD_SIZES,
             time_steps_pre_lockdown=180, time_steps_post_lockdown_pre_reopening=0, subsidy=0, num_trials=NUM_TRIALS,
-            dir_name_tag=DIR_NAME_TAG+'_same_age', timestamp=False)
+            dir_name_tag=DIR_NAME_TAG+'_same_age', timestamp=False, path='../results/')
