2a3900c2079e0147609f6e00b3ed86625db0004d
xflorameng <33183180+xflorameng@users.noreply.github.com>
2021-04-15 12:20:35 -0400

    Update overcrowding.py

--- analysis/overcrowding.py
+++ analysis/overcrowding.py
@@ -1,14 +1,18 @@
+import os
 import warnings
+from zipfile import ZipFile
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
 
-
 # COVID-19 deaths data from Hopkins Population Center (HPC)
 covid_deaths_date = '20200731'
 
+if not os.path.exists('../data/hpc_data_hub/Pandemic/casesAndDeaths.csv'):
+    with ZipFile('../data/hpc_data_hub/Pandemic.zip', 'r') as zip_obj:
+        zip_obj.extractall('../data/hpc_data_hub/')  # Extract all the contents in the same directory
 covid = pd.read_csv('../data/hpc_data_hub/Pandemic/casesAndDeaths.csv',
                     index_col=['fips'], usecols=['fips', 'stname', 'ctyname', 'deaths_' + covid_deaths_date])
 
@@ -68,36 +72,36 @@ df['covid_deaths_normalized'] = df['covid_deaths'] / df['county_population'] * 1
 df['percent_household_overcrowding'] = df['percent_household_overcrowding'] * 100
 
 # Filtering counties by rurality
-df = df.loc[df['percent_rural_population'] <= 5]
-county_rurality = 'mostly_urban'
-# df = df.loc[(df['percent_rural_population'] >= 45) & (df['percent_rural_population'] <= 55)]
-# county_rurality = 'urban_rural'
-# df = df.loc[df['percent_rural_population'] == 100]
-# county_rurality = 'rural'
+df_list = [df.loc[df['percent_rural_population'] <= 5],
+           df.loc[(df['percent_rural_population'] >= 45) & (df['percent_rural_population'] <= 55)],
+           df.loc[df['percent_rural_population'] == 100]
+           ]
+county_rurality_list = ['mostly_urban', 'urban_rural', 'rural']
 
-# Filtering states by the number of counties
-n_largest_states = 4
-n_counties_by_state = df.groupby('state').size()
-df = df.loc[df['state'].isin(n_counties_by_state.nlargest(n_largest_states).index)]
-
-warnings.filterwarnings('ignore')
-
-n_cols = n_largest_states
-fontsize = 18
-labelsize = fontsize
-plt.style.use('seaborn-white')
-g = sns.lmplot(x='percent_household_overcrowding', y='covid_deaths_normalized', data=df,
-               col='state', col_wrap=n_cols, robust=True, ci=95, n_boot=5000, height=4.5,
-               scatter_kws={'s': 160, 'color': mcolors.CSS4_COLORS['lightcoral']},
-               line_kws={'color': mcolors.CSS4_COLORS['darkred'], 'linewidth': 5})
-plt.xlim(0, 13)
-plt.ylim(-20, 400)
-axes = g.axes.flatten()
-for i in range(len(axes)):
-    axes[i].set_title(df.state.unique()[i], fontsize=fontsize)
-    axes[i].set_xlabel('Percent of household overcrowding', fontsize=fontsize)
-    axes[i].tick_params(labelsize=labelsize)
-    if i % n_cols == 0:
-        axes[i].set_ylabel(r'Deaths per $10^5$ people', fontsize=fontsize)
-plt.savefig(f'outputs/household_overcrowding_covid_deaths_by_state_{county_rurality}.pdf',
-            transparent=True, bbox_inches='tight')
+for df, county_rurality in zip(df_list, county_rurality_list):
+    # Filtering states by the number of counties
+    n_largest_states = 4
+    n_counties_by_state = df.groupby('state').size()
+    df = df.loc[df['state'].isin(n_counties_by_state.nlargest(n_largest_states).index)]
+    # Switch off warnings
+    warnings.filterwarnings('ignore')
+    # Plot
+    n_cols = n_largest_states
+    fontsize = 18
+    labelsize = fontsize
+    plt.style.use('seaborn-white')
+    g = sns.lmplot(x='percent_household_overcrowding', y='covid_deaths_normalized', data=df,
+                   col='state', col_wrap=n_cols, robust=True, ci=95, n_boot=5000, height=4.5,
+                   scatter_kws={'s': 160, 'color': mcolors.CSS4_COLORS['lightcoral']},
+                   line_kws={'color': mcolors.CSS4_COLORS['darkred'], 'linewidth': 5})
+    plt.xlim(0, 13)
+    plt.ylim(-20, 400)
+    axes = g.axes.flatten()
+    for i in range(len(axes)):
+        axes[i].set_title(df.state.unique()[i], fontsize=fontsize)
+        axes[i].set_xlabel('Percent of household overcrowding', fontsize=fontsize)
+        axes[i].tick_params(labelsize=labelsize)
+        if i % n_cols == 0:
+            axes[i].set_ylabel(r'Deaths per $10^5$ people', fontsize=fontsize)
+    plt.savefig(f'outputs/household_overcrowding_covid_deaths_by_state_{county_rurality}.pdf',
+                transparent=True, bbox_inches='tight')
