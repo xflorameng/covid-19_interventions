e07036064ea21a4f49a9b538cf51d07048b15420
xflorameng <33183180+xflorameng@users.noreply.github.com>
2021-04-15 13:41:15 -0400

    Update monte_carlo_simulation_lockdown.py

--- src/monte_carlo_simulation_lockdown.py
+++ src/monte_carlo_simulation_lockdown.py
@@ -10,9 +10,9 @@ NUM_TRIALS = 100
 
 monte_carlo(worker_staying_at_home_prob=np.around(np.arange(0, 1.1, 0.1), decimals=3),
             num_households=NUM_HOUSEHOLDS, max_household_sizes=MAX_HOUSEHOLD_SIZES, num_trials=NUM_TRIALS,
-            dir_name_tag=DIR_NAME_TAG, timestamp=False)
+            dir_name_tag=DIR_NAME_TAG, timestamp=False, path='../results/')
 
 # Robustness test of same-age households
 monte_carlo(same_age_household=True, worker_staying_at_home_prob=np.around(np.arange(0, 1.1, 0.1), decimals=3),
             num_households=NUM_HOUSEHOLDS, max_household_sizes=MAX_HOUSEHOLD_SIZES, num_trials=NUM_TRIALS,
-            dir_name_tag=DIR_NAME_TAG+'_same_age', timestamp=False)
+            dir_name_tag=DIR_NAME_TAG+'_same_age', timestamp=False, path='../results/')
