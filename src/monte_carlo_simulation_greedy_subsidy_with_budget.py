128c933761c1d78214942703042a86b311bd5a58
xflorameng <33183180+xflorameng@users.noreply.github.com>
2021-04-15 13:41:47 -0400

    Update monte_carlo_simulation_greedy_subsidy_with_budget.py

--- src/monte_carlo_simulation_greedy_subsidy_with_budget.py
+++ src/monte_carlo_simulation_greedy_subsidy_with_budget.py
@@ -15,4 +15,4 @@ monte_carlo(subsidy_population_factor=np.around(np.arange(.1, 1.1, 0.2), decimal
             worker_staying_at_home_prob=np.around(np.arange(0, 1.1, 0.1), decimals=3),
             allocation=ALLOCATION, subsidy=SUBSIDY,
             num_households=NUM_HOUSEHOLDS, max_household_sizes=MAX_HOUSEHOLD_SIZES, num_trials=NUM_TRIALS,
-            dir_name_tag=DIR_NAME_TAG, timestamp=False)
+            dir_name_tag=DIR_NAME_TAG, timestamp=False, path='../results/')
